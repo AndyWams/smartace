e1e603eefcfd804b13c1b383afe2b89e2658e157
AndyWams <andrew.wams@gmail.com>
2022-02-28 13:53:23 +0100

    update login screen mobile responsiveness

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  show: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  password() {
+    this.show = !this.show;
   }
-
 }
