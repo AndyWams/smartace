171ae4c0e778ab0dae985230a772e76068584ea6
AndyWams <andrew.wams@gmail.com>
2022-03-01 00:11:35 +0100

    completed dashboard translation

---
+++ src/app/portal/pages/dashboard/components/payroll-events/payroll-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payroll-events',
+  templateUrl: './payroll-events.component.html',
+  styleUrls: ['./payroll-events.component.scss']
+})
+export class PayrollEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/portal/pages/dashboard/dashboard.module.ts
+++ src/app/portal/pages/dashboard/dashboard.module.ts
@@ -4,10 +4,25 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { AngularMaterialModule } from '../../shared/angular-material.module';
+import { OverviewComponent } from './components/overview/overview.component';
+import { ChartGraphComponent } from './components/chart-graph/chart-graph.component';
+import { PayrollEventsComponent } from './components/payroll-events/payroll-events.component';
+import { ChartsModule, WavesModule } from 'angular-bootstrap-md';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule, AngularMaterialModule],
+  declarations: [
+    DashboardComponent,
+    OverviewComponent,
+    ChartGraphComponent,
+    PayrollEventsComponent,
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    AngularMaterialModule,
+    ChartsModule,
+    WavesModule,
+  ],
   exports: [DashboardComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
